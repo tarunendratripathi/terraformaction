b350bfbbd397cf8d9d226072f94ba1a86c94ffb1
Tarunendra Tripathi <tarunendra.info@gmail.com>
2025-07-16 12:21:47 +0530

    new

--- providers.tf
+++ providers.tf
@@ -1,8 +1,8 @@
 terraform {
  backend "azurerm" {
-   storage_account_name = "tflockstoragehim"
-   container_name = "tflockstoragehimcon1"
-   resource_group_name = "tf-lock"
+   storage_account_name = "julystorage1"
+   container_name = "julycontainer"
+   resource_group_name = "julyrsg"
    key = "githubactions/terraform.tfstate"   
  }
 }
